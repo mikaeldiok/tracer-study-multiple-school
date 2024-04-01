cc937f14a88c481d1d738d05d4500d13f67c1f5e
mikaeldiok <mikaeldiok@gmail.com>
2024-04-01 09:08:30 +0700

    student page

--- Modules/School/Resources/views/frontend/students/show.blade.php
+++ Modules/School/Resources/views/frontend/students/show.blade.php
@@ -1,103 +1,84 @@
-@extends('school::frontend.layouts.catalog')
+@extends('backend.layouts.app')
 
-@section('title') {{ __("Donatur") }} @endsection
+@section('title') {{ __($module_action) }} {{ $module_title }} @stop
+
+@section('breadcrumbs')
+<x-backend-breadcrumbs>
+    <x-backend-breadcrumb-item type="active" icon='{{ $module_icon }}'>{{ $module_title }}</x-backend-breadcrumb-item>
+</x-backend-breadcrumbs>
+@stop
 
 @section('content')
+<div class="card">
+    <div class="card-body">
+        <h3>Data Siswa</h3>
+        <table class="table">
+            <tbody>
+                <td>ID Data</td>
+                    <th id="student_id">: {{ $student->id }}</th>
+                </tr>
+                <tr>
+                    <td>Nama</td>
+                    <th id="name">: {{ $student->name }}</th>
+                </tr>
+                <tr>
+                    <td>Student ID</td>
+                    <th id="student_id">: {{ $student->student_id }}</th>
+                </tr>
+            </tbody>
+        </table>
+        <hr>
+        <div class="row">
+            <div class="col-8">
+                <h4 class="card-title mb-0">
+                    History Siswa
+                </h4>
+            </div>
+            <!--/.col-->
+            <div class="col-4">
+                <div class="float-right">
+                    @can('student_area')
+                        <x-buttons.create route='{{ route("frontend.$module_name_records.createSrRecords",$student) }}' title="{{__('Tambah Riwayat')}} {{ ucwords(Str::singular($module_name_records)) }}">Tambah Riwayat</x-buttons.create>
+                    @endcan
+                </div>
+            </div>
+            <!--/.col-->
+        </div>
+        <!--/.row-->
 
-<div class=" container z-2">
-    <div class="row">
-        <div class="col mb-5">
-            <button class="btn btn-sm btn-warning my-2" onclick="history.back()"><i class="fa-solid fa-arrow-left"></i> Kembali</button>
-            <div class="card bg-white border-light shadow-soft no-gutters p-4">
-                <div class="row">
-                    <div class="col-md-3 col-sm-12">
-                        <img src="{{$student->photo ? asset($student->photo) : asset('img/default-avatar.jpg') }}" class="img-thumbnail img-fluid" alt="Student image">
-                        <div class="row border m-2 p-1">
-                        <div class="col-12">
-                            <table>
-                            <tbody>
-                                <tr>
-                                    <td class="font-weight-bold">K1 </td>
-                                    <td>: {{$student->k1}}</td>
-                                </tr>
-                                <tr>
-                                    <td class="font-weight-bold">K2 </td>
-                                    <td>: {{$student->k2}}</td>
-                                </tr>
-                                <tr>
-                                    <td class="font-weight-bold">K3 </td>
-                                    <td>: {{$student->k3}}</td>
-                                </tr>
-                                <tr>
-                                    <td class="font-weight-bold">K4 </td>
-                                    <td>: {{$student->k4}}</td>
-                                </tr>
-                                <tr>
-                                    <td class="font-weight-bold">K5 </td>
-                                    <td>: {{$student->k5}}</td>
-                                </tr>
-                            </tbody>
-                            </table>
-                        </div>
-                    </div>
-                    </div>
-                    <div class="col-md-9 col-sm-12">
-                        <h2 class="display-5 mt-2 font-weight-bold" style="font-size:30px"> {{$student->name}} </h2>
-                        @php
-                            $birthdate = Carbon\Carbon::createFromFormat('Y-m-d', $student->birth_date);
-                            $age = $birthdate->diffInYears(Carbon\Carbon::now());
-                        @endphp
-                        <h3 class="display-6 mt-2"> {{$age}} Tahun</h3>
-                        <h4 class="display-6 mt-2"> {{$student->major}} - {{$student->year_class}} </h4>
-                        <table>
-                            <tbody>
-                                <tr>
-                                    <td class="font-weight-bold">Gender </td>
-                                    <td>: {{$student->gender}}</td>
-                                </tr>
-                                <tr>
-                                    <td class="font-weight-bold">Agama </td>
-                                    <td>: {{$student->religion}}</td>
-                                </tr>
-                                <tr>
-                                    <td class="font-weight-bold">TB/BB </td>
-                                    <td>: {{$student->height}} cm / {{$student->weight}} kg</td>
-                                </tr>
-                                <tr>
-                                    <td class="font-weight-bold">Sertifikasi </td>
-                                    <td>: {{$student->certificate}}</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <div class="my-3">
-                            @if($student->available)
-                                @if($student->checkBookedBy(auth()->user()->corporation->id ?? 0))
-                                    <button class="btn btn-lg btn-danger choose-student" data-id="{{$student->id}}" id="choose-student-{{$student->id}}">BATAL</button>
-                                @else
-                                    <button class="btn btn-lg btn-success choose-student" data-id="{{$student->id}}" id="choose-student-{{$student->id}}">PILIH</button>
-                                @endif
-                            @else
-                                @if($student->checkBookedBy(auth()->user()->corporation->id ?? 0))
-                                    <button class="btn btn-lg btn-danger choose-student with-warning" data-id="{{$student->id}}" id="choose-student-{{$student->id}}">Hapus Pilihan</button>
-                                @else
-                                    <div class="btn-lg btn-secondary-o disabled" id="">Currently Not Available</div>
-                                @endif
-                            @endif
-                        </div>
-                    </div>
+        <div class="row mt-4">
+            <div class="col">
+                <div class="table-responsive">
+                    <table class="table">
+                        {{ $dataTable->table() }}
+                    </table>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
-@endsection
+
+    <div class="card-footer">
+        <div class="row">
+            <div class="col">
+                <small class="float-right text-muted">
+                    Updated: {{$student->updated_at->diffForHumans()}},
+                    Created at: {{$student->created_at->isoFormat('LLLL')}}
+                </small>
+            </div>
+        </div>
+    </div>
+</div>
+@stop
 
 @push ('after-styles')
+<!-- DataTables School and Extensions -->
+<link rel="stylesheet" href="{{ asset('vendor/datatable/datatables.min.css') }}">
+
 @endpush
 
 @push ('after-scripts')
-
-@include("school::frontend.students.dynamic-scripts")
-
+<!-- DataTables School and Extensions -->
+{!! $dataTable->scripts()  !!}
 @endpush
+

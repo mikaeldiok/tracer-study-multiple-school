b7adb96a2d2ed34f0b46fcd4ff867b320ca1ff8b
mikaeldiok <mikaeldiok@gmail.com>
2024-05-09 22:25:53 +0700

    appends request to pagination

--- Modules/School/Resources/views/frontend/students/student-card-medium.blade.php
+++ Modules/School/Resources/views/frontend/students/student-card-medium.blade.php
@@ -3,7 +3,8 @@
     <div class="col-12 text-center">
         <img class="card-img-top p-1" src="{{$student->photo ? asset($student->photo) : asset('img/default-avatar.jpg') }}" alt="Image placeholder" style="max-height:190px;min-height:190px;object-fit: contain;">
         <h4 class="card-title pt-3" style="font-size: 16px">{{\Illuminate\Support\Str::limit($student->name, 30, $end = '...')}} </h4>
-
+        <small>{{config('unit-code')[$unit_origin_request]}}</small>
+        <p>{{$year_graduate_request}}</p>
     </div>
   </div>
 
